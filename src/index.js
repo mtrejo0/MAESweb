f14b8ceb0e281757925fe57aef9b21a08a7062fb
Moises Trejo <moisest@mit.edu>
2021-04-11 21:23:49 -0500

    added hash router

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import './styles/index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 render((
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>
+    </HashRouter>
 ), document.getElementById('root'));
